ba35ccf2502e11830b85154be0b74c91df430ecc
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2022-06-03 16:33:52 +0200

    add: base source code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# refactoring-glitchtip-log-server
+# Glitchtip Refactoring's Log Server
+
 GlitchTip makes monitoring software easy. Track errors, monitor performance, and check site uptime all in one place. Our app is compatible with Sentry client SDKs, but easier to run.
---
+++ example/node-js/index.js
@@ -0,0 +1,24 @@
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "http://localhost@o0.ingest.sentry.io/0",
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
